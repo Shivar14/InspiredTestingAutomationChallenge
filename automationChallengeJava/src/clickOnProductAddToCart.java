ceacc5c83e8115d38ae5a2ebd225a1c389a84faa
Shivar14 <shivzc@gmail.com>
2023-02-12 15:50:52 +0400

    InspiredTesting

--- automationChallengeJava/src/clickOnProductAddToCart.java
+++ automationChallengeJava/src/clickOnProductAddToCart.java
@@ -11,19 +11,19 @@ public class clickOnProductAddToCart {
 				System.setProperty("webdriver.chrome.driver", "/Users/shivarchetty/Downloads/chromedriver");		
 				WebDriver driver = new ChromeDriver();
 				
-				//Getting the website
+				//Getting to the website
 				driver.get("https://www.takealot.com/");
-				//Accepting the cookies
+				//Accepting the cookies on the page
 				driver.findElement(By.className("cookies-banner-module_dismiss-button_24Z98")).click();
-				//Clicking on search bar and entering a product
+				//Clicking on search bar,typing to search a product by name
 				driver.findElement(By.className("search-field")).sendKeys("PS5 Console");
-				//Entering on search bar to search product
+				//Entering on search bar to search that product
 				driver.findElement(By.className("search-field")).sendKeys(Keys.ENTER);
 				//Allowing wait time
 				Thread.sleep(1000);
-				//Clicking on product to open product page
+				//Clicking on product to open the product page
 				driver.findElement(By.id("70627462")).click();
-				//Adding product to cart
+				//Adding product to the cart
 				driver.findElement(By.cssSelector(".add-to-cart-button:first-of-type")).click();
 				
 
